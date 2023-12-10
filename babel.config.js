71154b9c1d0752e1c2ec18fc8b783b1eeef68d23
GSDV <sdvgab@gmail.com>
2023-12-09 21:45:40 -0600

    Basic FFMpeg Proj

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'], 
+      plugins: [
+          'expo-router/babel',
+          ["module-resolver", {
+              "alias": {
+                "@Navigation": "./src/navigation",
+                "@Components": "./src/components",
+                "@Screens": "./src/screens",
+                "@Stores": "./src/stores",
+                "@Assets": "./assets"
+              },
+              "extensions": [
+                ".js",
+                ".jsx",
+                ".ts",
+                ".tsx",
+              ]
+          }]
+      ]
+    };
 };
